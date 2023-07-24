77f77a994c99c8122094c93e6e174caa5bcca571
frynetworks <307862181+frynetworks@users.noreply.github.com>
2023-07-24 13:53:57 -0500

    Update GNSSMinerv1.py
    Hardcoded FTP information for the time being until a more secure approach can be done

--- GNSSMinerv1.py
+++ GNSSMinerv1.py
@@ -1,52 +1,56 @@
-import serial
 import datetime
 import os
 import pysftp
-import json
-from cryptography.fernet import Fernet
 import uuid
+from gps import *
+from time import *
 
-# Function to decrypt config file
-def decrypt_config():
-    with open("key.key", "rb") as key_file:
-        key = key_file.read()
-    cipher = Fernet(key)
-    with open("config.json.enc", "rb") as file:
-        encrypted_config = file.read()
-    config = json.loads(cipher.decrypt(encrypted_config))
-    return config
+gnss = gps(mode=WATCH_ENABLE)
 
-config = decrypt_config()
-
-ser = serial.Serial(port=config['serial_port'], baudrate=9600, timeout=1)
-
-# Get MAC address
 mac = '-'.join(['{:02x}'.format((uuid.getnode() >> i) & 0xff) for i in range(0,8*6,8)][::-1])
 
-def write_to_log(data, current_file):
+# FTP details
+config = {
+    "host": "207.244.74.204",
+    "username": "fryscrypto",
+    "password": "Wtf.7001",
+}
+
+last_upload_hour = datetime.datetime.now().hour
+
+# Initialize the current_file variable
+now = datetime.datetime.now()
+current_file = f"FRYgnss_{mac}_{now.strftime('%m%d%Y_%H%M%S')}.txt"
+
+def write_to_log(gnss_data, current_file):
     now = datetime.datetime.now()
     with open(current_file, 'a') as f:
-        f.write(f"{now.strftime('%H:%M:%S')} - {data}\n")
+        f.write(f"{now.strftime('%H:%M:%S')} - {gnss_data}\n")
 
-def upload_to_sftp(current_file):
-    remote_filename = f"/home/fryscrypto/indoor_gnss/{current_file}"
+def upload_to_sftp(current_file, config):
+    now = datetime.datetime.now()
+    local_filename = current_file
+    remote_filename = f"/home/fryscrypto/indoor_gnss/FRYgnss_{mac}_{now.strftime('%m%d%Y_%H%M%S')}.txt"
     cnopts = pysftp.CnOpts()
-    cnopts.hostkeys = None  # Disable host key checking.
+    cnopts.hostkeys = None 
     with pysftp.Connection(config['host'], username=config['username'], password=config['password'], cnopts=cnopts) as sftp:
-        sftp.put(current_file, remote_filename) 
-    os.remove(current_file)
+        sftp.put(local_filename, remote_filename)
+    os.remove(local_filename)
 
-now = datetime.datetime.now()
-current_file = f"FRYgnss_{mac}_{now.strftime('%m%d%Y_%H%M%S')}.log"
-
-last_upload_hour = now.hour
 while True:
-    data = ser.readline().decode('utf-8').strip()
-    if data:  # if data is not empty
-        print(f"Received: {data}")
-        write_to_log(data, current_file)
-        now = datetime.datetime.now()
-        if now.hour != last_upload_hour:
-            upload_to_sftp(current_file)
-            last_upload_hour = now.hour
-            current_file = f"FRYgnss_{mac}_{now.strftime('%m%d%Y_%H%M%S')}.log"
+    now = datetime.datetime.now()
+
+    # Every 10 seconds write GNSS data to file
+    if now.second % 10 == 0:
+        gnss_data = gnss.next()  # get gnss data
+        write_to_log(gnss_data, current_file)
+        print(f"Recorded GNSS data at {now.strftime('%H:%M:%S')}")  # printing for visibility
+    
+    # Upload the file one minute before the top of the hour
+    if now.minute == 59 and now.second == 0:
+        upload_to_sftp(current_file, config)
+
+    # Update the filename at the top of the hour
+    if now.minute == 0 and now.second == 0 and now.hour != last_upload_hour:
+        current_file = f"FRYgnss_{mac}_{now.strftime('%m%d%Y_%H%M%S')}.txt"
+        last_upload_hour = now.hour
